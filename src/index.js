a9d0ce3490e1f1683e178ad8bc2c38a71f44798e
Harritone <yourrasik@gmail.com>
2020-09-26 12:09:39 +1000

    Added snackbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Tooltip from './main-ui/tooltip';
 import Dropdown from './main-ui/dropdown';
 import Tabs from './main-ui/tabs';
+import Snackbar from './main-ui/snackbar';
 
 // create a tooltip
 
@@ -18,3 +19,11 @@ dropdowns.forEach(dropdown => {
 // create tabs
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.initialize();
+
+//create snackbar
+const snackbar = new Snackbar();
+snackbar.initialize();
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me');
+});
