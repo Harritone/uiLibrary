b06b998f1d35ab7d38a2ea59f88ba3985711fddd
Harritone <yourrasik@gmail.com>
2020-09-26 10:51:10 +1000

    Added dropdowns

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Tooltip from './main-ui/tooltip';
+import Dropdown from './main-ui/dropdown';
 
 // create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.initialize();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.initialize();
+})
